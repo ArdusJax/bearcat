1012abc62ea09e60d77d60ff39be9dfaddafaca6
Christopher O'Brian <mobiletread@gmail.com>
2019-11-08 05:40:49 -0800

    Flushing out the functionality

---
+++ src/bucket.rs
@@ -0,0 +1,9 @@
+
+// Download contents to an S3 bucket
+pub fn download(client: &rusoto_s3::S3Client, path: String) -> Result<(),()> {
+
+}
+// Upload contents to an S3 bucket
+pub fn upload(client: &rusoto_s3::S3Client, path: String, bucket: String) -> Result<(),()> {
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 #[macro_use]
 extern crate clap;
+extern crate rusoto_s3;
+extern crate rusoto_core;
 
+mod bucket;
 
 use clap::{App, ArgMatches};
+use rusoto_s3::{S3Client};
+use rusoto_core::{region};
+use bucket::{upload, download};
 
 // Flow of the application
 // Set the AWS profile for the commercial role with bucket access
@@ -13,25 +19,21 @@ fn main() {
     let yaml = load_yaml!("cli.yml");
     let app = App::from_yaml(yaml).version("0.1.0");
     let matches = app.get_matches().clone();
-
     let profile= matches.value_of("profile");
-    //let config = matches.value_of("config").unwrap().to_owned();
-    //let username = matches.value_of("verbose").unwrap().to_owned();
 
-    // you could do this
     if let Some(config) = matches.value_of("config") {
-        println!("You have a config file: {}", config)
+        println!("Using the config file: {}", config)
     } else {
-        println!("Dude... seriously??!! No config file?!")
+        println!("No config file found. Using the defaults")
     }
 
-    // or this
-    let username = matches.value_of("username").unwrap_or("fred");
-    println!("Username: {}", username);
-
-    // or maybe this... so many choices for you!
     match profile {
         Some(p) => println!("You provided the profile, {}", p),
-        None => println!("WTF... you need a profile bro!!"),
+        None => println!("No profile provided. Falling back to using IAM default"),
     }
+
+    let client = rusoto_s3::S3Client::new(region::Region::UsWest2);
+    let download_result = download(&client, String::from("path"));
+    let upload_result= download(&client, String::from("path"));
+
 }
