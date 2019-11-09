4ab220d63bca35476b06d584f7165184c013b417
Christopher O'Brian <mobiletread@gmail.com>
2019-11-09 14:23:16 -0800

    fixing the return for the upload and download methods of bucket.rs

--- src/bucket.rs
+++ src/bucket.rs
@@ -2,10 +2,11 @@
 // Download contents to an S3 bucket
 // Todo: Add actual type safe results
 pub fn download(client: &rusoto_s3::S3Client, path: String) -> Result<(),()> {
-
+    Ok(())
 }
+
 // Upload contents to an S3 bucket
 // Todo: Add actual type safe results
 pub fn upload(client: &rusoto_s3::S3Client, path: String, bucket: String) -> Result<(),()> {
-
+    Ok(())
 }
