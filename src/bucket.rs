10fd65a9f6a410ae83a762cd1e6c18af94e7d55d
Christopher O'Brian <mobiletread@gmail.com>
2019-11-17 22:05:04 -0800

    Adding file parsing

--- src/bucket.rs
+++ src/bucket.rs
@@ -1,9 +1,13 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
 use rusoto_s3::{GetObjectRequest, CreateMultipartUploadRequest, UploadPartRequest};
 use crate::rusoto_s3::S3;
+
 // Download contents to an S3 bucket
 // Todo: Add actual type safe results
-pub fn download(client: &rusoto_s3::S3Client, path: String, bucket: &String) -> Result<(),()> {
-  let req = GetObjectRequest{ bucket: bucket.clone(), ..Default::default()};
+pub fn download<'a,'b>(client: &rusoto_s3::S3Client, path: &'a str, bucket: &'b str) -> Result<(),()> {
+  let req = GetObjectRequest{ bucket: String::from(bucket), ..Default::default()};
   let res = client.get_object(req);
   
     Ok(())
@@ -11,28 +15,45 @@ pub fn download(client: &rusoto_s3::S3Client, path: String, bucket: &String) -> 
 
 // Upload using multipart method, the contents to an S3 bucket
 // Todo: Add actual type safe results
-pub fn upload(client: &rusoto_s3::S3Client, path: String, filename: &String, bucket: &String) -> Result<(),()> {
+pub fn upload<'a,'b,'c>(client: &rusoto_s3::S3Client, path: &'a str, filename: &'b str, bucket: &'c str) -> Result<(),()> {
+  // Create the request for a multiport upload to the S3 bucket
   let req = CreateMultipartUploadRequest {
-    bucket: bucket.clone(),
-    key: filename.clone(),
+    bucket: bucket.to_owned(),
+    key: filename.to_owned(),
     ..Default::default()
   };
+
+  // Make the request and log the result
   let res = client.create_multipart_upload(req)
   .sync()
   .expect("Could not create multipart upload.");
   println!("{:#?}", res);
+  // Get the upload id from the resposne
   let upload_id = res.upload_id.unwrap();
 
+  // Function to create the parts to upload to the S3 bucket
   let create_upload_part = |body: Vec<u8>, part_number: i64| -> UploadPartRequest {
         UploadPartRequest {
             body: Some(body.into()),
             bucket: bucket.to_owned(),
             key: filename.to_owned(),
             upload_id: upload_id.to_owned(),
-            part_number: part_number,
+            part_number,
             ..Default::default()
         }
     };
 
+  Ok(())
+}
+
+// Processses the file and return a vec of vec<u8>
+pub fn processes_object(path: &str) -> std::io::Result<()> {
+  let file = File::open(path)?;
+  let mut reader = BufReader::with_capacity(10, file);
+
+  while !reader.fill_buf()?.is_empty() {
+    println!("{:?}", reader.buffer());
+    reader.consume(10);
+  }
   Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ mod bucket;
 use clap::{App, ArgMatches};
 use rusoto_s3::{S3Client};
 use rusoto_core::{region};
-use bucket::{upload, download};
+use bucket::{upload, download, processes_object};
 
 // Flow of the application
 // Set the AWS profile for the commercial role with bucket access
@@ -19,12 +19,24 @@ fn main() {
     let yaml = load_yaml!("cli.yml");
     let app = App::from_yaml(yaml).version("0.1.0");
     let matches = app.get_matches().clone();
-    let profile= matches.value_of("profile");
+    let profile = matches.value_of("profile");
+    let source_bucket = matches.value_of("source");
+    let destination_bucket = matches.value_of("destination");
 
     if let Some(config) = matches.value_of("config") {
         println!("Using the config file: {}", config)
     } else {
         println!("No config file found. Using the defaults")
+    }
+
+    match source_bucket {
+        Some(p) => println!("You provided the source bucket, {}", p),
+        None => panic!("No source bucket provided. You need to specify a source bucket"),
+    }
+
+    match destination_bucket {
+        Some(p) => println!("You provided the destination bucket, {}", p),
+        None => panic!("No destination bucket provided. You need to specify a source bucket"),
     }
 
     match profile {
@@ -34,9 +46,10 @@ fn main() {
 
     // The path, region, etc... will come from environment variables, command line args or can be
     // parsed out of a config file if that is present.
-    let bucket = "doa-test-bucket".to_owned();
-    let client = rusoto_s3::S3Client::new(region::Region::UsWest2);
-    let download_result = download(&client, String::from("path"), &bucket);
-    let upload_result= download(&client, String::from("path"), &bucket);
-
+    let sour_bucket = "bearcat-commercial";
+    let dest_bucket = "bearcat-commercial";
+    //let client = rusoto_s3::S3Client::new(region::Region::UsEast1);
+    //let download_result = download(&client, "path", sour_bucket);
+    //let upload_result= upload(&client, "path", "test.txt", dest_bucket);
+    let res = processes_object("test.txt");
 }
