abb524b7f9bbbf3ac70f4731ee757a19ffa3cc64
Christopher O'Brian <mobiletread@gmail.com>
2019-11-17 23:18:06 -0800

    Update the processing
    Iterate over the contents of a file and get the UploadPartRequests ready for the multipart upload

--- src/bucket.rs
+++ src/bucket.rs
@@ -30,8 +30,21 @@ pub fn upload<'a,'b,'c>(client: &rusoto_s3::S3Client, path: &'a str, filename: &
   println!("{:#?}", res);
   // Get the upload id from the resposne
   let upload_id = res.upload_id.unwrap();
+  // Create all of the parts for uploading
+  processes_object(filename, bucket, filename, &upload_id);
 
-  // Function to create the parts to upload to the S3 bucket
+  Ok(())
+}
+
+// Processses the file and return a vec of UploadPartRequest
+pub fn processes_object(path: &str, bucket: &str, filename: &str, upload_id: &str) -> Result<Vec<UploadPartRequest>, std::io::Error>  {
+  // Get the file ready for processing
+  let file = File::open(path)?;
+  let mut reader = BufReader::with_capacity(10, file);
+  // Get all of the parameters for the upload parts initialized
+  let index = 0;
+  let mut upload_requests: Vec<UploadPartRequest>;
+  // Anonymous function for creating the UploadPartRequest
   let create_upload_part = |body: Vec<u8>, part_number: i64| -> UploadPartRequest {
         UploadPartRequest {
             body: Some(body.into()),
@@ -42,18 +55,13 @@ pub fn upload<'a,'b,'c>(client: &rusoto_s3::S3Client, path: &'a str, filename: &
             ..Default::default()
         }
     };
-
-  Ok(())
-}
-
-// Processses the file and return a vec of vec<u8>
-pub fn processes_object(path: &str) -> std::io::Result<()> {
-  let file = File::open(path)?;
-  let mut reader = BufReader::with_capacity(10, file);
-
+  // Start parsing the file and creating the parts, collecting them in a vector
   while !reader.fill_buf()?.is_empty() {
     println!("{:?}", reader.buffer());
+    upload_requests.push(create_upload_part(reader.buffer().to_vec(), index));
     reader.consume(10);
+    index = index + 1;
   }
-  Ok(())
+  // If there are no errors return the vec of UploadPartRequest
+  Ok(upload_requests)
 }
