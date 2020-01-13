782fcd799bc7ccab29991bf0e5a3f5713477cabb
Christopher O'Brian <chris.o-brian@sap.com>
2020-01-13 14:14:48 -0800

    updating setup.sh

--- resources/setup.sh
+++ resources/setup.sh
@@ -7,8 +7,9 @@ SOURCE_BUCKET=$2
 KEY_NAME=$3
 
 # Check if the aws cli is installed
-installed="which aws"
-[$installed -eq 0] && echo "aws cli is installed..." || echo "aws cli could not be found or is not installed. Please install the cli and try this script again."
+command -v aws >/dev/null 2>&1 || { echo >&2 "AWS cli could not be found or is not installed. Please install the cli and try this script again."; exit 1; }
+echo "AWS cli found..."
 
 # Execute the cloud formation with the given parameters
+echo "Executing cloudformation..."
 aws cloudformation create-stack --stack-name bearcat-sync --template-body file:///resources/bearcat.yaml --parameters ParameterKey=SourceBucketName,ParameterValue=$SOURCE_BUCKET,ParameterKey=DestinationBucketName,ParameterValue=$DESTINATION_BUCKET,ParameterKey=KeyName,ParameterValue=$KEY_NAME
