7a47de8c845ce0087522177d412339928e9658f0
Christopher O'Brian <mobiletread@gmail.com>
2020-01-20 22:14:02 -0800

    update credentials

--- src/credentials.rs
+++ src/credentials.rs
@@ -1,24 +1,40 @@
-use rusoto_core::Region;
+use rusoto_core::{Region, RusotoError};
+use rusoto_credential::AwsCredentials;
 use rusoto_sts::{StsAssumeRoleSessionCredentialsProvider, StsClient};
+use rusoto_ssm::*;
+use std::io::{Error, ErrorKind};
 
-enum Provider {
-    AWS_STS,
+pub enum CredentialProvider {
+    AwsSsm { key: String, region: Region },
 }
 
-// Assume the role used for syncing a bucket on the commercial side
-fn new_provider(provider_type: Provider, role_arn: &str, profile: &str, region: Region) {
-    let sts = StsClient::new(region);
-    let provider = StsAssumeRoleSessionCredentialsProvider::new(
-        sts,
-        role_arn.to_owned(),
-        profile.to_owned(),
-        None,
-        None,
-        None,
-        None,
-    );
-
-    let auto_refreshing_provider = rusoto_credential::AutoRefreshingProvider::new(provider);
+impl CredentialProvider {
+    pub fn get_credentials(self) -> Result<AwsCredentials, Error> {
+        match self {
+            Self::AwsSsm { key, region } => get_creds_from_ssm(key, region),
+        }
+    }
 }
 
-fn new_chain() {}
+// Get the credentials from ssm
+fn get_creds_from_ssm(key: String, region: Region) -> Result<AwsCredentials, Error> {
+    // need real logging not println
+    // need to include error chaining
+    let client = rusoto_ssm::SsmClient::new(region);
+    let req = GetParameterRequest {
+        name: key,
+        with_decryption: Some(true), // forcing always on for now
+    };
+    let res = client
+        .get_parameter(req)
+        .sync()
+        .expect("failed to get secret from parameter store");
+
+    let v = res.parameter.unwrap().value.unwrap();
+    let cred = parse_cred_from_ssm_value(&v);
+    Ok(AwsCredentials::new("something", "secret", None, None))
+}
+
+fn parse_cred_from_ssm_value(value: &str) -> Result<AwsCredentials, ()> {
+    Ok(AwsCredentials::new("something", "secret", None, None))
+}
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ extern crate rusoto_core;
 extern crate rusoto_credential;
 extern crate rusoto_s3;
 extern crate rusoto_sts;
+extern crate rusoto_ssm;
 extern crate futures;
 extern crate bytes;
 
@@ -16,6 +17,8 @@ use rusoto_core::region::Region;
 use rusoto_s3::S3Client;
 use std::str::FromStr;
 use std::env;
+
+use credentials::{CredentialProvider};
 // Flow of the application
 // Set the AWS profile for the commercial role with bucket access
 // Download the contents of the commercial bucket
@@ -28,9 +31,9 @@ fn main() {
     let profile = matches.value_of("profile");
     let source_bucket = matches.value_of("source");
     let destination_bucket = matches.value_of("destination");
-    let source_region = matches.value_of("source_region").unwrap_or("us-west-2");
+    let source_region = matches.value_of("region_source").unwrap_or("us-west-2");
     let destination_region = matches
-        .value_of("destination_region")
+        .value_of("region_destination")
         .unwrap_or("us-west-2");
 
     if let Some(config) = matches.value_of("config") {
@@ -54,41 +57,48 @@ fn main() {
         None => println!("No profile provided. Falling back to using IAM default"),
     }
 
+    // Testing
+    let cred_prov = CredentialProvider::AwsSsm {
+        key: "bearcat_test".to_owned(),
+        region: Region::from_str(&source_region).unwrap(),
+    };
+
+    cred_prov.get_credentials()
     // The path, region, etc... will come from environment variables, command line args or can be
     // parsed out of a config file if that is present.
-    let client = rusoto_s3::S3Client::new(Region::from_str(&source_region).unwrap());
+    //let client = rusoto_s3::S3Client::new(Region::from_str(&source_region).unwrap());
     // If there are objects in the bucket then get all of the objects and
     // sync them over to the destination bucket
-    if let Ok(file_names) = get_bucket_object_keys(&client, source_bucket.unwrap()) {
-        for file_name in file_names {
-            // Download the artifact from the source S3 bucket
-            let download_result = download(&client, &file_name, source_bucket.unwrap());
-            match download_result {
-                Ok(res) => println!(
-                    "Download of {:?} completed successfully!\n{:?}",
-                    file_name, res
-                ),
-                Err(e) => panic!(format!(
-                    "Download of {:?} failed with error:\n{:?}",
-                    file_name, e
-                )),
-            }
-            // Set to the destination profile
-            let up_profile = "AWS_PROFILE";
-            env::set_var(up_profile, "destination");
+    //if let Ok(file_names) = get_bucket_object_keys(&client, source_bucket.unwrap()) {
+    //    for file_name in file_names {
+    //        // Download the artifact from the source S3 bucket
+    //        let download_result = download(&client, &file_name, source_bucket.unwrap());
+    //        match download_result {
+    //            Ok(res) => println!(
+    //                "Download of {:?} completed successfully!\n{:?}",
+    //                file_name, res
+    //            ),
+    //            Err(e) => panic!(format!(
+    //                "Download of {:?} failed with error:\n{:?}",
+    //                file_name, e
+    //            )),
+    //        }
+    //        // Set to the destination profile
+    //        let up_profile = "AWS_PROFILE";
+    //        env::set_var(up_profile, "destination");
 
-            // Upload the artifact from the local machine to the destination bucket
-            let upload_client = rusoto_s3::S3Client::new(Region::from_str(&source_region).unwrap());
-            let upload_result = upload(
-                &upload_client,
-                "data",
-                &file_name,
-                destination_bucket.unwrap(),
-            );
-            match upload_result {
-                Ok(res) => println!("Upload was successful!\n{:?}", res),
-                Err(e) => panic!(format!("Could not upload artifact...\nError:\n{:?}", e)),
-            }
-        }
-    }
+    //        // Upload the artifact from the local machine to the destination bucket
+    //        let upload_client = rusoto_s3::S3Client::new(Region::from_str(&source_region).unwrap());
+    //        let upload_result = upload(
+    //            &upload_client,
+    //            "data",
+    //            &file_name,
+    //            destination_bucket.unwrap(),
+    //        );
+    //        match upload_result {
+    //            Ok(res) => println!("Upload was successful!\n{:?}", res),
+    //            Err(e) => panic!(format!("Could not upload artifact...\nError:\n{:?}", e)),
+    //        }
+    //    }
+    //}
 }
